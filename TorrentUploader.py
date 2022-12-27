536cff69f7b456e795aae93f3e66d27a8804a504
Karako <karakohear@gmail.com>
2022-12-27 14:14:52 +0800

    ✨ remove useless file 🎨 format code

--- TorrentUploader.py
+++ TorrentUploader.py
@@ -4,7 +4,13 @@ from PyQt5.QtWidgets import QApplication
 
 from windows.wndMain import WndMain
 
-app = QApplication(sys.argv)
-mainwindow = WndMain()
-mainwindow.show()
-sys.exit(app.exec_())
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = WndMain()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
