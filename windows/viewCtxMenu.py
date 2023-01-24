b0866d16183e0a39a1bb3a560bf1b8316f3e9ade
barryZZJ <850462618@qq.com>
2023-01-24 21:54:12 +0800

    update action menu status tip

--- windows/viewCtxMenu.py
+++ windows/viewCtxMenu.py
@@ -10,22 +10,23 @@ class ViewContextMenu(QMenu):
         self.pubtype = pubtype
         self.actPubMore = QAction(ICONS.PUBMORE, '编辑并发布...', parent)
         self.actPubMore.setObjectName('actPubMore')
-        self.actPubMore.setStatusTip('进一步编辑后再发布（一次只能选择一行）')
+        self.actPubMore.setStatusTip('进一步编辑后再发布（只能选择一行）')
 
         self.actPubDirect = QAction(ICONS.PUBDIRECT, '一键发布', parent)
         self.actPubDirect.setObjectName('actPubDirect')
-        self.actPubDirect.setStatusTip('不进行确认，直接发布（可以一次处理多行）')
+        self.actPubDirect.setStatusTip('不进行确认，直接发布（支持多选）')
 
         self.actMakeBTDetail = QAction(ICONS.MAKEBT, '制作种子（显示详情）', parent)
         self.actMakeBTDetail.setObjectName('actMakeBTDetail')
-        self.actMakeBTDetail.setStatusTip('显示制作种子的详情页面')
+        self.actMakeBTDetail.setStatusTip('显示制作种子的详情页面（支持多选）')
 
         self.actMakeBTSilent = QAction(ICONS.MAKEBT, '制作种子（跳过详情）', parent)
         self.actMakeBTSilent.setObjectName('actMakeBTSilent')
-        self.actMakeBTSilent.setStatusTip('跳过详情页面（会沿用上次制作时的配置，请确认是否正在做种)')
+        self.actMakeBTSilent.setStatusTip('跳过详情页面（支持多选）（会沿用上次制作时的配置，请确认是否正在做种）')
 
         self.actMoveTo = QAction(ICONS.MOVETO, '移动到' + ('已发布' if pubtype == PubType.Todo else '待发布'), parent)
         self.actMoveTo.setObjectName('actMoveTo')
+        self.actMakeBTSilent.setStatusTip('支持多选')
 
         self.actOpen = QAction(ICONS.OPEN, '打开所在文件夹', parent)
         self.actOpen.setObjectName('actOpen')
